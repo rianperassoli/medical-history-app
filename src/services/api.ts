625d16de83bae5bee035d8237f0ef3d1aa35dfef
Rian Perassoli <rian.perassoli@gmail.com>
2022-05-09 00:52:18 -0300

    add local api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://medical-history-api.herokuapp.com'
-  // baseURL: 'http://localhost:3000'
+  // baseURL: 'https://medical-history-api.herokuapp.com'
+  baseURL: 'http://localhost:3000'
 })

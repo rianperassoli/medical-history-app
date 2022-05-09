2621bcc759ce5362031e5c85626910acc780afa1
Rian Perassoli <rian.perassoli@gmail.com>
2022-05-09 11:42:00 -0300

    fixed to work with localhost

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://medical-history-api.herokuapp.com'
-  // baseURL: 'http://localhost:3000'
+  baseURL: 'http://localhost:3000'
 })
